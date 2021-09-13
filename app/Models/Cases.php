6d5525bd93ba3656b16b1156cbd2a25b881e26a8
Abdulbasid <lbaseed2010@gmail.com>
2021-09-13 23:02:25 +0100

    changes

--- app/Models/Cases.php
+++ app/Models/Cases.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Cases extends Model
+class cases extends Model
 {
     use HasFactory;
 
